fe3ca941ad10395879efbbc865bb8d129c97946d
ilas-pakaya <ikhlaspakaya73@gmail.com>
2025-06-05 11:04:48 +0700

    class node

--- stack.cpp
+++ stack.cpp
@@ -24,3 +24,17 @@ class Stack
     Stack(){
         top = NULL; // Initalize the stack with a null top pointer
     }
+
+
+    // Push operation: Insert an element onto the top of the stuck
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. set the next pointer of the new node to the current top
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+   
